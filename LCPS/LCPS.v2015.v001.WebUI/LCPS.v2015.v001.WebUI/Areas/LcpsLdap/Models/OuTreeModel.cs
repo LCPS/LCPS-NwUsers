78fdaee3a171cb0169780d8dd650ffb83a44708c
The Modulator <earlyms@SBO-C003-01.lcps.local>
2015-07-22 15:56:40 -0400

    Added Home Page for LDAP

---
+++ LCPS/LCPS.v2015.v001.WebUI/LCPS.v2015.v001.WebUI/Areas/LcpsLdap/Models/OuTreeModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Anvil.v2015.v001.Domain.Html;
+using LCPS.v2015.v001.NwUsers.LcpsLdap.LdapObjects;
+
+
+namespace LCPS.v2015.v001.WebUI.Areas.LcpsLdap.Models
+{
+    public class OuTreeModel : IAnvilFormHandler
+    {
+        private LcpsAdsOuTree _ouTree;
+
+        public string FormArea { get; set; }
+
+        public string FormController { get; set; }
+
+        public string FormAction { get; set; }
+
+        public string SubmitText { get; set; }
+
+        public string OnErrorActionName { get; set; }
+
+        public LcpsAdsOuTree OuTree
+        {
+            get
+            {
+                if (_ouTree == null)
+                    _ouTree = new LcpsAdsOuTree();
+
+                return _ouTree;
+            }
+        }
+
+    }
+}
